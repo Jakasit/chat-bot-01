0797386599def70b3ef933401dabd7f0b21574e5
Tent <jakasit@gmail.com>
2022-07-11 16:24:22 +0700

    make it better

---
+++ intent.py
@@ -0,0 +1,16 @@
+{'responseId': '92c9b9c5-d762-4f7b-b68b-a38319b09f40-b4cb2fcf',
+ 'queryResult': {'queryText': 'รหัส 001 จำนวน 10', 'action': 
+'intent-buy.intent-buy-custom',
+'parameters': {'id_item': 1.0, 'number': 10.0}, 'allRequiredParamsPresent': True, 'fulfillmentMessages': [{'text': {'text': ['']}}], 'intent': {'name': 'projects/chat-bot-project-ajnq/agent/intents/02d03f3d-62e1-429d-a14c-621f335f446c', 'displayName': 'intent-buy - custom'}, 'intentDetectionConfidence': 1.0, 'languageCode': 'th'}, 'originalDetectIntentRequest': {'source': 'line', 'payload': {'data': {'timestamp': '1630916430713', 'replyToken': '0a0354df374047dda010ce16aff476cb', 'type': 'message', 'source': {'userId': 'Ue0231792530c51e0245860cca4f5f857', 'type': 'user'}, 'message': {'id': '14699531611422', 'type': 'text', 'text': 'รหัส 001 จำนวน 10'}}}}, 'session': 'projects/chat-bot-project-ajnq/agent/
+sessions/bb1d1343-9b56-3b96-abe0-4bd95421c91c'}
+
+{'responseId': '9d188f5f-2dac-4bcd-ac60-c17e2f165d38-b4cb2fcf', 
+'queryResult': {'queryText': 'รหัส 001 จำนวน 10', 'action': 
+'intent-buy.intent-buy-custom', 
+'parameters': {'id_item': 1.0, 'number': 10.0}, 'allRequiredParamsPresent': True, 'fulfillmentMessages': [{'text': {'text': ['']}}], 
+'outputContexts': [{'name': 'projects/chat-bot-project-ajnq/agent/sessions/bb1d1343-9b56-3b96-abe0-4bd95421c91c/contexts/intent-buy-followup', 'lifespanCount': 5, 'parameters': {'id_item': 1.0, 
+'id_item.original': '001', 'number': 10.0,
+'number.original': '10'}}], 'intent': {'name': 'projects/chat-bot-project-ajnq/agent/intents/02d03f3d-62e1-429d-a14c-621f335f446c', 'displayName': 'intent-buy - custom'}, 'intentDetectionConfidence': 1.0, 'languageCode': 'th'}, 
+'originalDetectIntentRequest': {'source': 'line', 'payload': {'data': {'type': 'message', 'message': {'id': '14699840784907', 'text': 'รหัส 001 จำนวน 10', 'type': 'text'}, 'timestamp': '1630920152032', 'source': {'userId': 'Ue0231792530c5
+1e0245860cca4f5f857', 'type': 'user'}, 'replyToken': '4da8045ce6d448ad958dd2a6e8aa73dd'}}}, 'session': 'projects/chat-bot-project-ajnq/agent/sessions/bb1d1343-9b56-3b96-abe0-4bd95421c91c'}
+
